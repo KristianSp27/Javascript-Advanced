68589a3cd237f480902b373e9d22c0193f6b33ce
KristianSp27 <101999488+KristianSp27@users.noreply.github.com>
2023-01-26 22:43:52 +0200

    New commit

---
+++ heroicInventory.js
@@ -0,0 +1,11 @@
+function heroicInventory(array) {
+  let result = [];
+
+  for (const iterator of array) {
+    let [name, level, items] = iterator.split(" / ");
+    level = Number(level);
+    items = items ? items.split(", ") : [];
+    result.push({ name, level, items });
+  }
+  console.log(JSON.stringify(result));
+}
